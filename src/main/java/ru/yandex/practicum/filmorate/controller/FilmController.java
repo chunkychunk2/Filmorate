185fab72b722f0f32f4b4c2aaa43d106a16ac815
chun <chunoff@list.ru>
2025-04-07 22:17:00 +0300

    выполнил основное задание

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,90 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/films")
+@Slf4j
+public class FilmController {
+
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        log.info("Получен запрос на получение списка всех фильмов.");
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@RequestBody Film film) {
+        log.info("Получен запрос на добавление фильма: {}", film);
+        validateFilm(film);
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        log.info("Добавлен фильм: {}", film);
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@RequestBody Film newFilm) {
+        log.info("Получен запрос на обновление фильма: {}", newFilm);
+        if (newFilm.getId() == null) {
+            log.warn("Id должен быть указан");
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+        if (films.containsKey(newFilm.getId())) {
+            Film oldFilm = films.get(newFilm.getId());
+            if (newFilm.getName() == null || newFilm.getDuration() == null || newFilm.getReleaseDate() == null ||
+                    newFilm.getDescription() == null) {
+                return oldFilm;
+            }
+            validateFilm(newFilm);
+            oldFilm.setDescription(newFilm.getDescription());
+            oldFilm.setDuration(newFilm.getDuration());
+            oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            oldFilm.setName(newFilm.getName());
+            log.info("Обновлен фильм: {}", oldFilm);
+            return oldFilm;
+        }
+        log.warn("Фильм с id = " + newFilm.getId() + " не найден");
+        throw new NotFoundException("Фильм с id = " + newFilm.getId() + " не найден");
+    }
+
+    public void validateFilm(Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            log.warn("Название не может быть пустым");
+            throw new ValidationException("Название не может быть пустым");
+        }
+        if (film.getDescription() != null && film.getDescription().length() > 200) {
+            log.warn("Максимальная длина описания — 200 символов");
+            throw new ValidationException("Максимальная длина описания — 200 символов");
+        }
+        if (film.getReleaseDate() == null || film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.warn("дата релиза — не раньше 28 декабря 1895 года");
+            throw new ValidationException("дата релиза — не раньше 28 декабря 1895 года");
+        }
+        if (film.getDuration() == null || film.getDuration() <= 0) {
+            log.warn("Продолжительность фильма должна быть положительным числом.");
+            throw new ValidationException("Продолжительность фильма должна быть положительным числом.");
+        }
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,100 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
+import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    private final Map<Long, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> findAll() {
+        log.info("Получен запрос на получение списка всех пользователей.");
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        log.info("Получен запрос на добавление пользователя: {}", user);
+        validateUser(user);
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+        log.info("Добавлен пользователь: {}", user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser) {
+        log.info("Получен запрос на обновление пользователя: {}", newUser);
+        if (newUser.getId() == null) {
+            log.warn("Id должен быть указан");
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+        if (users.containsKey(newUser.getId())) {
+            User oldUser = users.get(newUser.getId());
+            if (newUser.getEmail() == null || newUser.getBirthday() == null || newUser.getLogin() == null ||
+                    newUser.getName() == null) {
+                return oldUser;
+            }
+            validateUser(newUser);
+            oldUser.setEmail(newUser.getEmail());
+            oldUser.setBirthday(newUser.getBirthday());
+            oldUser.setName(newUser.getName());
+            oldUser.setLogin(newUser.getLogin());
+            log.info("Обновлен пользователь: {}", oldUser);
+            return oldUser;
+        }
+        log.warn("Юзер с id = " + newUser.getId() + " не найден");
+        throw new NotFoundException("Юзер с id = " + newUser.getId() + " не найден");
+    }
+
+    public void validateUser(User user) {
+        if (users.values().stream()
+                .anyMatch(u -> !u.getId().equals(user.getId()) && u.getEmail().equals(user.getEmail()))) {
+            log.warn("Этот имейл уже используется другим пользователем");
+            throw new DuplicatedDataException("Этот имейл уже используется другим пользователем");
+        }
+        if (users.values().stream()
+                .anyMatch(u -> !u.getId().equals(user.getId()) && u.getLogin().equals(user.getLogin()))) {
+            log.warn("Этот логин уже используется другим пользователем");
+            throw new DuplicatedDataException("Этот логин уже используется другим пользователем");
+        }
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            log.warn("электронная почта не может быть пустой и должна содержать символ @");
+            throw new ValidationException("электронная почта не может быть пустой и должна содержать символ @");
+        }
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            log.warn("логин не может быть пустым и содержать пробелы");
+            throw new ValidationException("логин не может быть пустым и содержать пробелы");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.warn("Дата рождения не может быть в будущем");
+            throw new ValidationException("Дата рождения не может быть в будущем");
+        }
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
